d0edd44e7ed27e555eed9201a1b0a0c6f651f55f
UserMarcus <114788534+UserMarcus@users.noreply.github.com>
2023-07-06 14:51:28 +0800

    Add files via upload

---
+++ PersonalInformation.py
@@ -0,0 +1,27 @@
+
+print("Programmer: Marcus Duane C. Santos")
+print()
+
+print("Date Created: July 6, 2023")
+print("Description of the Program: This program shows the information of a person.")
+
+print()
+print("First Name: Marcus  Duane")
+print("Middle Name: Cucio")
+print("Last Name: Santos")
+print("Course: BS Information Technology")
+print("Year: 3rd Year")
+print("Age: 21")
+print("Gender: Male")
+print("Civil Status: Single")
+print("Nationality: Filipino")
+print()
+print("Motto: Ang Buhay parang Life ingatan mo yan")
+print()
+
+print("School: PHINMA Araullo University")
+print("College: CITE")
+
+
+
+
