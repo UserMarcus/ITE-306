b36a4b2b6b1e9f76a61f2eacb975ba517f4dea24
UserMarcus <114788534+UserMarcus@users.noreply.github.com>
2023-07-08 15:49:37 +0800

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+
+# Ask the user to enter a given sample area of width and length of the rectangle to solve the area.
+
+width = float(input("Enter a given width of the rectangle: "))
+length = float(input("Enter a given length of the rectangle: "))
+
+area = width * length
+
+print("Area of rectangle is = ",area)
